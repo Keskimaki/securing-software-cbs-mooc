de6014fc233a5b7c1b17a951b4a162b7898e0f83
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-02-19 13:30:36 +0200

    Calculator

--- part1-03.calculator/src/pages/views.py
+++ part1-03.calculator/src/pages/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 
 
-# Create your views here.
-
 def addPageView(request):
-	return HttpResponse('0')
-	
+    first = int(request.GET.get('first'))
+    second = int(request.GET.get('second'))
+
+    return HttpResponse(first + second)	
 
 def multiplyPageView(request):
-	return HttpResponse('0')
+    first = int(request.GET.get('first'))
+    second = int(request.GET.get('second'))
+
+    return HttpResponse(first * second)
