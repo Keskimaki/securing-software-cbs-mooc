6e16f6c5c1f7861e34bb1b3d0ca79fe16a2a07af
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-02-20 11:31:15 +0200

    Hello Web with Database

--- part2-10.hellowebwithdatabase/src/pages/views.py
+++ part2-10.hellowebwithdatabase/src/pages/views.py
@@ -5,7 +5,12 @@ from .models import Message
 # Create your views here.
 
 def homePageView(request):
-	content = 'Hello Web!';
+	content = 'Hello Web!'
 
+	id = request.GET.get('id')
+
+	if id:
+		id = int(id)
+		content = Message.objects.get(pk=id).content
 		
 	return HttpResponse(content)
